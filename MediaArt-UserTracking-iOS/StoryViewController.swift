bc3fc557d712db4f0f887e311ff79ba5e0feee86
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2014-10-28 23:20:25 +0900

    storiesを定義

--- MediaArt-UserTracking-iOS/StoryViewController.swift
+++ MediaArt-UserTracking-iOS/StoryViewController.swift
@@ -9,27 +9,26 @@
 import UIKit
 
 class StoryViewController: UIViewController {
+    @IBOutlet weak var storyTextView: UITextView!
+
+    typealias storiesType = Dictionary<String, String>
+    
+    let stories: [storiesType] = [
+        [
+            "story": "zero"
+        ],
+        [
+            "story": ""
+        ]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.storyTextView.text = stories[0]["story"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
